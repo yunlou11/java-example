5d29a7ced5c087f38ba8bd6778ff7525f9e9fa5f
dongyunlong <dongyunlong@pwrd.com>
2018-05-11 16:51:53 +0800

    最佳买卖股票时机

--- leetcode/src/main/java/com/dyl/array/MaxProfit.java
+++ leetcode/src/main/java/com/dyl/array/MaxProfit.java
@@ -9,19 +9,17 @@ public class MaxProfit {
         if(prices.length==0){
             return 0;
         }
-        int[] profit = new int[prices.length];
-        profit[0] = 0;
+        int profit = 0;
         for(int i=1;i<prices.length;i++){
-            profit[i] = profit[i-1];
             if(prices[i] > prices[i-1]){
-                profit[i] += (prices[i] - prices[i-1]);
+                profit += (prices[i] - prices[i-1]);
             }
         }
-        return profit[profit.length-1];
+        return profit;
     }
 
     public static void main(String[] args){
-        int[] prices = new int[]{7,6,4,3,1};
+        int[] prices = new int[]{1,6,4,3,1};
         System.out.println(maxProfit(prices));
     }
 }
